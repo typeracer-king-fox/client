703bb552204cc651cefa9b8a6aed81b9e17ca506
Dwitama Alfred <dwitama.alfred@gmail.com>
2019-12-12 17:10:04 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,26 @@
 <template>
   <div id="app">
+    <h1>{{this.$store.state.lintasan[0].content}}</h1>
+    <!-- <h1>ahusdhkaushdka</h1> -->
     <router-view/>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase/app'
-
-require("firebase/firestore")
-
-  var firebaseConfig = {
-    apiKey: "AIzaSyDcowEaERZ0TpZVtt1V_mJqhX3ZJoMIUuo",
-    authDomain: "typeracer-kingfox.firebaseapp.com",
-    databaseURL: "https://typeracer-kingfox.firebaseio.com",
-    projectId: "typeracer-kingfox",
-    storageBucket: "typeracer-kingfox.appspot.com",
-    messagingSenderId: "342306038905",
-    appId: "1:342306038905:web:7942bf507373dcce38a0a8",
-    measurementId: "G-MNJQPBFSCP"
-  }
-
-  firebase.initializeApp(firebaseConfig)
-  let db = firebase.firestore()
-
+  import {mapState} from '../src/store/index'
   export default {
-    name : 'typeracer',
     data(){
       return {
-        lintasan : [],
         player : [],
       }
     },
     methods: {
-      getData(){
-        db.collection('lintasan').onSnapshot(querySnapshot => {
-          querySnapshot.forEach(lintasanData => {
-            
-            this.lintasan.push(lintasanData.data())
-            })
-            console.log(this.lintasan[0].content)
-        })
-      }
+
     },
-    mounted(){
-      this.getData()
-    },
+    created(){
+      this.$store.dispatch('getData')
+    }
+
   }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase/app'
 
 Vue.use(Vuex)
 
+require("firebase/firestore")
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDcowEaERZ0TpZVtt1V_mJqhX3ZJoMIUuo",
+    authDomain: "typeracer-kingfox.firebaseapp.com",
+    databaseURL: "https://typeracer-kingfox.firebaseio.com",
+    projectId: "typeracer-kingfox",
+    storageBucket: "typeracer-kingfox.appspot.com",
+    messagingSenderId: "342306038905",
+    appId: "1:342306038905:web:7942bf507373dcce38a0a8",
+    measurementId: "G-MNJQPBFSCP"
+  }
+
+  
+  firebase.initializeApp(firebaseConfig)
+  let db = firebase.firestore()
+
 export default new Vuex.Store({
   state: {
+    lintasan : []
   },
   mutations: {
+    GET_DATA(state,payload){
+      state.lintasan = payload
+    }
   },
   actions: {
+    getData({commit}){
+      let lintasan = []
+      db.collection('lintasan').onSnapshot(querySnapshot => {
+        querySnapshot.forEach(lintasanData => {
+          lintasan.push(lintasanData.data())
+          })
+      })
+      commit('GET_DATA',lintasan)
+    }
   },
   modules: {
   }
